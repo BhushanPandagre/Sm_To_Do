395301fb0a6ae7d5369b3ce340884b389065aea4
Bhushan pandagre <dengercoder790@gmail.com>
2024-12-22 23:17:15 +0530

    first commit

---
+++ client/redux-toolkit/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodos from './components/AddTodos';
+import Todos from './components/Todos'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Code With Bhushan </h1>
+     <AddTodos/>
+     <Todos/>
+    </>
+  )
+}
+
+export default App
